512280285e13f9d6d3439cc4550906a7becd6d72
UNWOMEN <hypolite.reoutar@unwomen.org>
2026-04-07 04:24:48 +0100

    Update env.js

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 window.NEARCONNECT_SUPABASE_URL = 'https://vtezfkktbrtdnxeayjjm.supabase.co';
-window.NEARCONNECT_SUPABASE_ANON_KEY = 'NearConnect@2026_Kigali';
+window.NEARCONNECT_SUPABASE_ANON_KEY = 'TA_CLE_ANON_ICI';
 window.NEARCONNECT_FUNCTIONS_BASE = 'https://vtezfkktbrtdnxeayjjm.supabase.co/functions/v1';
 window.NEARCONNECT_ADMIN_MASTER_KEY = 'NearConnect@2026';
