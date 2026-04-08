3a89e3bbd374cbe28dd71fb2ad696e1a143659ed
UNWOMEN <hypolite.reoutar@unwomen.org>
2026-04-08 03:33:10 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,6 +3,10 @@ window.sb = supabase.createClient(
   window.NEARCONNECT_SUPABASE_ANON_KEY,
   {
     auth: { persistSession: false },
-    global: { headers: { apikey: window.NEARCONNECT_SUPABASE_ANON_KEY } },
+    global: {
+      headers: {
+        apikey: window.NEARCONNECT_SUPABASE_ANON_KEY,
+      },
+    },
   }
 );
